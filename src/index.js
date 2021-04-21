662b846793ad57e9f5a41eaafbe02176ec6f52e2
Agam Kashyap <agamkashyap2018@gmail.com>
2021-04-22 00:40:30 +0530

    Lamp positions corrected, made more realistic

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import * as THREE from 'https://unpkg.com/three@0.127.0/build/three.module.js';
 import { OrbitControls } from 'https://unpkg.com/three@0.127.0/examples/jsm/controls/OrbitControls.js'
 import { OBJLoader } from 'https://unpkg.com/three@0.127.0/examples/jsm/loaders/OBJLoader.js';
 
-let camera, controls, scene, renderer;
+var controls, scene, renderer;
 
 let CAMERA_STRUCT = {
 	fp_camera : new THREE.PerspectiveCamera( 30, window.innerWidth / window.innerHeight, 0.1, 1000 ),
@@ -21,7 +21,7 @@ function init() {
 	// Scene Setup
 	scene = new THREE.Scene();
 	scene.background = new THREE.Color( 0x000000 );
-	scene.fog = new THREE.FogExp2( 0xcccccc, 0.002 );
+	// scene.fog = new THREE.FogExp2( 0xcccccc, 0.001 );
 
 	const loadingManager = new THREE.LoadingManager( () => {
 	
@@ -54,7 +54,7 @@ function init() {
 	controls.minDistance = 100;
 	controls.maxDistance = 500;
 
-	controls.maxPolarAngle = Math.PI / 2;
+	controls.maxPolarAngle = Math.PI / 2.2;
 
 	// world
 
@@ -62,7 +62,7 @@ function init() {
 	let material = new THREE.MeshPhongMaterial( { color: 0x808080});
 	let plane = new THREE.Mesh(plane_geo, material)
 	plane.rotation.x = - Math.PI * 0.5;
-	plane.position.y = -10
+	// plane.position.y = -10
 	plane.receiveShadow = true;
 	scene.add(plane);
 
@@ -72,7 +72,8 @@ function init() {
 	// x+z = 200
 	// x+z = -200
 	// 
-	for ( let i = 0; i < 20; i ++ ) {
+	let objcnt=1;
+	for (let i = 0; i < 20; i ++ ) {
 
 		let x = -300 + i*100
 		let z = 200 - x;
@@ -81,46 +82,57 @@ function init() {
 		loader.load( '../assets/street-lamp-1/street-lamp.obj', function ( obj ) {
 
 			object = obj;
+			
 			object.scale.multiplyScalar( 0.1 );
-			object.position.x = x
-			object.position.y = 0;
-			object.position.z = z;
+			object.position.x = -x
+			object.position.y = 50;
+			object.position.z = -z;
 			object.rotation.y = -Math.PI/4;
 			object.updateMatrix();
 			object.matrixAutoUpdate = false;
 			object.castShadow = true;		
-		
-			let spotLight = new THREE.SpotLight( 0xffffff, 1 );
-			spotLight.position.y = 100;
-			spotLight.position.x = 10;
-			spotLight.angle = Math.PI / 4;
-			spotLight.penumbra = 0.1;
+
+			let name = "Lamp ".concat(objcnt);
+			object.name = name;
+
+			let spotLight = new THREE.SpotLight( 0xffe692, 1 );
+			spotLight.position.y = 1100;
+			spotLight.position.x = 450;
+			spotLight.angle = Math.PI / 6;
+			spotLight.penumbra = 0.2;
 			spotLight.decay = 2;
-			spotLight.distance = 1200;
-			spotLight.intensity = 2;
+			spotLight.distance = 1000;
+			spotLight.intensity = 1;
 
 			spotLight.castShadow = true;
-			spotLight.shadow.mapSize.width = 512;
-			spotLight.shadow.mapSize.height = 512;
+			spotLight.shadow.mapSize.width = 212;
+			spotLight.shadow.mapSize.height = 212;
 			spotLight.shadow.camera.near = 10;
 			spotLight.shadow.camera.far = 200;
 			spotLight.shadow.focus = 1;
 
-			const sphere = new THREE.SphereGeometry( 2.5, 16, 8 );
-			spotLight.add(new THREE.Mesh(sphere, new THREE.MeshBasicMaterial( { color: 0xffffff })));
+			spotLight.name = "Light".concat(toString(objcnt));
+
+			let pointLight =  new THREE.PointLight( 0xffe692, 2, 100 );
+			spotLight.add(pointLight);
+
+			const sphere = new THREE.SphereGeometry( 50, 16, 8 );
+			spotLight.add(new THREE.Mesh(sphere, new THREE.MeshBasicMaterial( { color: 0xffe692 })));
 			let target = new THREE.Object3D();
-			target.position.x = x+100;
-			target.position.z = z+100;
+			target.position.x = -x+50;
+			target.position.z = -z+50;
 			scene.add(target);
 			spotLight.target = target;
-			object.add(spotLight);
-
+			// object.add(spotLight);
+			object.children[0].add(spotLight);
 			scene.add( object );
+
+			objcnt+=1;
 		});
 	}
 
 	const box = new THREE.BoxGeometry(60,60,60);
-	const box_material = new THREE.MeshPhongMaterial({color: 0x00ff00, flatShading: true});
+	const box_material = new THREE.MeshPhongMaterial({color: 0xffffff, flatShading: true});
 	const cube = new THREE.Mesh(box, box_material);
 	cube.position.y = 30;
 	cube.position.x = 0;
@@ -134,9 +146,13 @@ function init() {
 	dirLight1.position.set( 1, 1, 1 );
 	// scene.add( dirLight1 );
 
-	const dirLight2 = new THREE.DirectionalLight( 0x002288 );
-	dirLight2.position.set( - 1, - 1, - 1 );
-	// scene.add( dirLight2 );
+	const dirLight2 = new THREE.DirectionalLight( 0x111111 );
+	// dirLight2.position.set( 0, 1, 1 );
+	dirLight2.position.y = 10;
+	dirLight2.position.x = 10;
+	dirLight2.position.z = 10;
+	
+	scene.add( dirLight2 );
 
 	const ambient = new THREE.AmbientLight( 0xffffff, 0.1 );
 	scene.add( ambient );
@@ -145,6 +161,7 @@ function init() {
 
 	window.addEventListener( 'resize', onWindowResize );
 
+	console.log(scene);
 }
 
 function onWindowResize() {
